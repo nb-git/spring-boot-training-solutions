a07fb80a5bfc99408f695294785a2071210710c5
David Wanaki <david.wanaki.extern@innogy.com>
2018-07-05 15:56:49 +0200

    Add persistence layer

--- beersupplier/src/main/java/de/innogy/emobility/springtraining/beersupplier/service/BeerService.java
+++ beersupplier/src/main/java/de/innogy/emobility/springtraining/beersupplier/service/BeerService.java
@@ -2,7 +2,9 @@ package de.innogy.emobility.springtraining.beersupplier.service;
 
 import de.innogy.emobility.springtraining.beersupplier.exception.NotInStockException;
 import de.innogy.emobility.springtraining.beersupplier.model.Beer;
+import de.innogy.emobility.springtraining.beersupplier.repository.BeerRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -15,30 +17,24 @@ import java.util.Map;
 @Service
 public class BeerService {
 
-    private Map<String, Beer> availableBeer;
+    @Autowired
+    private BeerRepository beerRepository;
 
     @PostConstruct
     public void init() {
-        availableBeer = new HashMap<>();
-        availableBeer.put("Snake Venom",
-                          Beer.builder().alcoholVol(67.5).name("Snake Venom").bottleSizeInMl(500).build());
-        availableBeer.put("Sink the Bismarck!",
-                          Beer.builder().alcoholVol(41.0).name("Sink the Bismarck!").bottleSizeInMl(375).build());
-        availableBeer
-                .put("Innogy Pils", Beer.builder().alcoholVol(5.0).name("Innogy Pils").bottleSizeInMl(500).build());
-        availableBeer.put("Innogy Bock",
-                          Beer.builder().alcoholVol(8.5).name("Innogy Bock").bottleSizeInMl(5000).build());
-        availableBeer.put("Faxe", Beer.builder().alcoholVol(5.0).name("Faxe").bottleSizeInMl(1000).build());
-        availableBeer.put("Elephant Beer",
-                          Beer.builder().alcoholVol(11.0).name("Elephant Beer").bottleSizeInMl(500).build());
-        availableBeer.put("Innogy Radler",
-                          Beer.builder().alcoholVol(2.5).name("Innogy Radler").bottleSizeInMl(500).build());
-        availableBeer.put("Innogy Alkoholfrei",
-                          Beer.builder().alcoholVol(0.0).name("Innogy Alkoholfrei").bottleSizeInMl(500).build());
+        List<Beer> beers = new ArrayList<>();
+        beers.add(Beer.builder().alcoholVol(67.5).name("Snake Venom").bottleSizeInMl(500).build());
+        beers.add(Beer.builder().alcoholVol(41.0).name("Sink the Bismarck!").bottleSizeInMl(375).build());
+        beers.add(Beer.builder().alcoholVol(5.0).name("Innogy Pils").bottleSizeInMl(500).build());
+        beers.add(Beer.builder().alcoholVol(8.5).name("Innogy Bock").bottleSizeInMl(5000).build());
+        beers.add(Beer.builder().alcoholVol(5.0).name("Faxe").bottleSizeInMl(1000).build());
+        beers.add(Beer.builder().alcoholVol(11.0).name("Elephant Beer").bottleSizeInMl(500).build());
+        beers.add(Beer.builder().alcoholVol(2.5).name("Innogy Radler").bottleSizeInMl(500).build());
+        beers.add(Beer.builder().alcoholVol(0.0).name("Innogy Alkoholfrei").bottleSizeInMl(500).build());
     }
 
     public Beer provideBeerByName(String name) throws NotInStockException {
-        Beer providedBeer = availableBeer.get(name);
+        Beer providedBeer = beerRepository.findById(name).orElse(null);
         if(providedBeer == null){
             log.info(name + "  is not listed.");
             throw new NotInStockException(name + " is not listed.");
@@ -47,27 +43,29 @@ public class BeerService {
     }
 
     public List<Beer> provideStock() {
-        return new ArrayList<>(availableBeer.values());
+        return beerRepository.findAll();
     }
 
     public void addToStock(Beer beer) {
-        availableBeer.put(beer.getName(), beer);
+        beerRepository.save(beer);
         log.info(beer.getName() + " was added to stock.");
     }
 
     public void removeBeer(String beerName) throws NotInStockException {
-        Beer removedBeer = availableBeer.remove(beerName);
+        Beer removedBeer = beerRepository.findById(beerName).orElse(null);
         if(removedBeer == null){
             log.info(beerName + " cannot be removed as it is not listed.");
             throw new NotInStockException(beerName + " cannot be removed as it is not listed.");
+        } else {
+            beerRepository.deleteById(beerName);
         }
         log.info(beerName + " was removed from stock.");
     }
 
     public void updateBeer(Beer beer) throws NotInStockException {
-        availableBeer.put(provideBeerByName(beer.getName()).getName(), beer);
+        provideBeerByName(beer.getName());
+        beerRepository.save(beer);
         log.info(beer.getName() + " was updated.");
-
     }
 
 }
