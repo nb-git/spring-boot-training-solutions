aea3f69db73f362bd1e5830e8ed74c8af5ff19cf
Niels Buchholz <niels.buchholz.extern@innogy.com>
2018-07-10 10:03:39 +0200

    Rename application

--- beershop/src/main/java/de/innogy/emobility/springtraining/beershop/service/RabbitService.java
+++ beershop/src/main/java/de/innogy/emobility/springtraining/beershop/service/RabbitService.java
@@ -1,14 +1,11 @@
 package de.innogy.emobility.springtraining.beershop.service;
 
 import de.innogy.emobility.springtraining.beershop.controller.DeliveryDTO;
-import de.innogy.emobility.springtraining.beershop.controller.OrderDTO;
 import de.innogy.emobility.springtraining.beershop.model.BeerItem;
 import de.innogy.emobility.springtraining.beershop.repository.BeerItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
