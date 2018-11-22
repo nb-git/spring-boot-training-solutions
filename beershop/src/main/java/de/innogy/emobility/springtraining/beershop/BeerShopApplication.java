c1291ca050b9798ae59dd3536d1e11785e3ad2bf
Niels Buchholz <niels.buchholz.extern@innogy.com>
2018-11-22 12:25:27 +0100

    Add user provided service lesson

--- beershop/src/main/java/de/innogy/emobility/springtraining/beershop/BeerShopApplication.java
+++ beershop/src/main/java/de/innogy/emobility/springtraining/beershop/BeerShopApplication.java
@@ -3,8 +3,8 @@ package de.innogy.emobility.springtraining.beershop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TODO: Lesson 4.1 Enable Discovery Client
-//TODO: Lesson 4.2 Enable Circuit Breaker
+//TODO: Lesson 4.3 Enable Discovery Client
+//TODO: Lesson 4.4 Enable Circuit Breaker
 @SpringBootApplication
 public class BeerShopApplication {
 
