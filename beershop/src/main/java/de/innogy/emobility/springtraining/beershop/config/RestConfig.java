e7f54f88c6a5a053bcc44670c5dead4966d87e16
Niels Buchholz <niels.buchholz.extern@innogy.com>
2018-11-23 15:21:47 +0100

    Revert "fix rabbittests"
    This reverts commit 0bcc1e1a4056fb6f9240945fecab94590ef53e0c.

--- beershop/src/main/java/de/innogy/emobility/springtraining/beershop/config/RestConfig.java
+++ beershop/src/main/java/de/innogy/emobility/springtraining/beershop/config/RestConfig.java
@@ -1,5 +1,6 @@
 package de.innogy.emobility.springtraining.beershop.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
+    //TODO: Lesson 4.3 make it LoadBalanced to be able to use discovery service names
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- beersupplier/src/main/java/de/innogy/emobility/springtraining/beersupplier/BeerSupplierApplication.java
+++ beersupplier/src/main/java/de/innogy/emobility/springtraining/beersupplier/BeerSupplierApplication.java
@@ -3,8 +3,11 @@ package de.innogy.emobility.springtraining.beersupplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//Lesson 4.3 Enable Discovery Client
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableDiscoveryClient
 public class BeerSupplierApplication {
 
 	public static void main(String[] args) {
