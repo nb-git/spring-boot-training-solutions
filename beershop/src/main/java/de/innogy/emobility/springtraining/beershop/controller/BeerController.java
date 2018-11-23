0bcc1e1a4056fb6f9240945fecab94590ef53e0c
Niels Buchholz <niels.buchholz.extern@innogy.com>
2018-11-23 14:29:33 +0100

    fix rabbittests

--- beershop/src/main/java/de/innogy/emobility/springtraining/beershop/controller/BeerController.java
+++ beershop/src/main/java/de/innogy/emobility/springtraining/beershop/controller/BeerController.java
@@ -1,12 +1,9 @@
 package de.innogy.emobility.springtraining.beershop.controller;
 
-import com.google.common.collect.Lists;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import de.innogy.emobility.springtraining.beershop.exception.OutOfBeerException;
 import de.innogy.emobility.springtraining.beershop.model.BeerItem;
 import de.innogy.emobility.springtraining.beershop.service.SupplyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,26 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class BeerController {
 
     private SupplyService supplyService;
-    /*
-     * Lesson 4.4: On a Get-Request, request all available Beers from supplier,
-     * if the supplier is not reachable provide a list of surrogate beers.
-     */
-    @Value("${beersupplier.supply.all.url}")
-    private String beerProducerBeersUrl;
-
-    private RestTemplate restTemplate;
 
     @Autowired
     public BeerController(SupplyService supplyService, RestTemplate restTemplate) {
         this.supplyService = supplyService;
-        this.restTemplate = restTemplate;
     }
 
     @PostMapping("/order")
@@ -44,21 +31,6 @@ public class BeerController {
     @GetMapping("/menu")
     public List<BeerItem> getMenu() {
         return supplyService.provideMenu();
-    }
-
-    //Lesson 4.4 Restmethod
-    @HystrixCommand(fallbackMethod = "provideFallback")
-    @GetMapping("/fallback")
-    public List<BeerItem> getFallbackMenu() {
-        BeerItem[] beerItems = restTemplate.getForObject(beerProducerBeersUrl, BeerItem[].class);
-        return beerItems != null ? Arrays.asList(beerItems) : null;
-    }
-
-    //Lesson 4.4 Fallback-method
-    private List<BeerItem> provideFallback(){
-        BeerItem beer1 = new BeerItem("Wasser", 0.0, 1);
-        BeerItem beer2 = new BeerItem("Plörrbräu", 8.4, 1);
-        return Lists.newArrayList(beer1, beer2);
     }
 
 }
